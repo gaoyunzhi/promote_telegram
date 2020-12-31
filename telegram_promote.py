52618001ec3881cfdef830412068fc1d4903202e
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-31 11:38:33 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -16,22 +16,26 @@ with open('settings') as f:
     settings = yaml.load(f, Loader=yaml.FullLoader)
 
 async def run():
-	client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
+    client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
     await client.start(password=credential['password'])
     dialogs = await client.get_dialogs()
-    print('dialogs', dialogs)
-    print('dialogs[3]', dialogs[3])
+    for dialog in dialogs:
+        if dialog.name.startswith('中国自由'):
+            print(dialog)
 
     for target in settings:
-    	channel_entity=await client.get_entity(target)
-    	posts = await client(GetHistoryRequest(peer=channel_entity, limit=10,
-        	offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
-    	print(posts[0])
-    	 
+        target = target.split('(')[0]
+        channel_entity=await client.get_entity(target)
+        print('channel_entity', channel_entity, channel_entity.id)
+        posts = await client(GetHistoryRequest(peer=channel_entity, limit=10,
+            offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        print('posts', posts)
+        print('posts.messages[0]', posts.messages[0])
+
     await client.disconnect()
 
 if __name__ == "__main__":
-	loop = asyncio.new_event_loop()
+    loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     r = loop.run_until_complete(run())
     loop.close()
