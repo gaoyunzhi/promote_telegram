1ced73393d70dabb02a5a4ece2002e6ac8730a14
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-31 12:18:27 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -9,7 +9,12 @@ import plain_db
 from datetime import datetime
 import time
 
-existing = plain_db.loadKeyOnlyDB('existing')
+existing = plain_db.load('existing')
+last_send = {}
+
+for key, value in existing.items():
+    target = int(key.split('_')[0])
+    last_send[target] = max(last_send.get(target, 0), value)
 
 with open('credential') as f:
     credential = yaml.load(f, Loader=yaml.FullLoader)
@@ -20,26 +25,41 @@ with open('settings') as f:
 def shouldSend(messages):
     if time.time() - datetime.timestamp(messages[0].date) < 30 * 60:
         return False # 不打断现有对话
+    if time.time() - datetime.timestamp(messages[0].date) > 48 * 60 * 60:
+        return True
     for message in messages:
-        print(message.from_id)
-        print(message.from)
-    return False
+        if message.from_id.user_id == credential['user_id']:
+            return False
+    return True
 
+def getTarget(target):
+    target = target.split('(')[0]
+    try:
+        return int(target)
+    except:
+        return target
 
 async def run():
     client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
     await client.start(password=credential['password'])
 
-    for target in settings:
-        target = target.split('(')[0]
-        group=await client.get_entity(int(target))
-        print('group', group, group.id)
+    for target, setting in settings.items():
+        target = getTarget(target)
+        if time.time() - last_send.get(target, 0) < 48 * 60 * 60: # start with 48 hour, see if I can change this to 5 hour
+            continue
+
+        group=await client.get_entity(target)
+
+        print('group', group.name)
+        
         posts = await client(GetHistoryRequest(peer=group, limit=10,
             offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
         
         if not shouldSend(posts.messages):
             continue
-        print('posts.messages[0]', posts.messages[0])
+
+        setting['subscriptions']
+        
 
     await client.disconnect()
 
