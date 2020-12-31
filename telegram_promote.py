74996f2b973fabdde2b81f2b64225b657fcc1739
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-31 11:32:48 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -5,39 +5,30 @@ from telethon import TelegramClient
 from telethon.tl.functions.messages import GetHistoryRequest
 import yaml
 import asyncio
+import plain_db
+
+existing = plain_db.loadKeyOnlyDB('existing')
 
 with open('credential') as f:
     credential = yaml.load(f, Loader=yaml.FullLoader)
 
-async def findLinksAsync(source):
-    client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
+with open('settings') as f:
+    settings = yaml.load(f, Loader=yaml.FullLoader)
+
+async def run():
+	client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
     await client.start(password=credential['password'])
-    channel_entity=await client.get_entity(source)
-    posts = await client(GetHistoryRequest(peer=channel_entity, limit=30, # change to 30
-        offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+    dialogs = await client.get_dialogs()
+    print('dialogs', dialogs)
+    print('dialogs[3]', dialogs[3])
+
+    for target in settings:
+    	channel_entity=await client.get_entity(target)
+    	posts = await client(GetHistoryRequest(peer=channel_entity, limit=10,
+        	offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+    	print(posts[0])
+    	 
     await client.disconnect()
-    return posts
-
-def findLinks(source):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    r = loop.run_until_complete(findLinksAsync(source))
-    loop.close()
-    links = {}
-    for message in r.messages:
-        try:
-            message.media.webpage
-            message.media.webpage.title
-        except:
-            continue
-        links[tuple([item.url for item in message.entities])] = (
-            message.media.webpage.title[11:-21]).strip()
-        if len(links) == 8:
-            return links
-    return links
-
-def run():
-	...
 
 if __name__ == "__main__":
 	loop = asyncio.new_event_loop()
