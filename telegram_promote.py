ddbabb6760f2d63b49385b3a83744715d48fdb8c
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-03 05:45:03 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -98,7 +98,6 @@ async def process(client):
     random.shuffle(targets)
     for target, setting in targets:
         target = getTarget(target)
-        print('debug_gap_hour', target, time.time() - group_log.get(target, 0), setting.get('gap_hour', 5) * 60 * 60)
         if time.time() - group_log.get(target, 0) < setting.get('gap_hour', 5) * 60 * 60:
             continue
         if not added_time.get(target):
