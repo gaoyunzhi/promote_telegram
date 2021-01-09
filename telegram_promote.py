ed69de8404fa1cb158efb820280b2dc6f747951d
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-08 16:51:54 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -221,11 +221,21 @@ async def populateSetting(client):
         f.write(yaml.dump(settings, sort_keys=True, indent=2, allow_unicode=True))
 
 
+async def test(client):
+    await populateCache(client, 'freedom_watch')
+    posts = posts_cache['freedom_watch']
+    for post in posts:
+        if post.id != 3247:
+            continue
+        debug_group = await client.get_entity(credential['debug_group'])
+        await trySend(client, debug_group, 'freedom_watch', post)
+
 async def run():
     client = TelegramClient('session_file', credential['api_id'], credential['api_hash'])
     await client.start(password=credential['password'])
     await populateSetting(client)
-    await process(client)
+    await test(client)
+    # await process(client)
     await client.disconnect()
 
 if __name__ == "__main__":
