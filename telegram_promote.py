3b3d9cca1a5f905b08cd45178df71c1c8626f85b
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-14 08:51:28 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -69,6 +69,8 @@ def getPromoteMessageHash(message):
 def getMessageHash(post):
     message_id = post.grouped_id
     if post.fwd_from:
+        if post.fwd_from.from_id == None:
+            print('post.fwd_from.from_id None', post.fwd_from)
         message_id = message_id or post.fwd_from.channel_post
         return '%d=%s' % (getPeerId(post.fwd_from.from_id), str(message_id))
     message_id = message_id or post.id
