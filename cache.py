218615c07999e0ba4afa6efd655d55d3c48a5a61
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-15 20:01:50 -0800

    commit

--- cache.py
+++ cache.py
@@ -6,16 +6,20 @@ class Cache(object):
         self.entities = {}
         self.channels = {}
 
-    async def get_entity(self, client, eid):
-        if eid not in self.entities:
-            self.entities[eid] = await client.get_entity(eid)
-        return self.entities[eid]
+    async def get_entity(self, client, eid, readOnly=False):
+        if readOnly:
+            key = eid
+        else:
+            key = hash(client) + str(eid) 
+        if key not in self.entities:
+            self.entities[key] = await client.get_entity(eid)
+        return self.entities[key]
 
     async def getPosts(self, client, subscription, S):
         if subscription not in self.posts:
             if subscription not in S.settings['id_map']:
                 await S.populateIdMap(client, subscription)
-            self.channels[subscription] = await self.get_entity(client, S.settings['id_map'][subscription])
+            self.channels[subscription] = await self.get_entity(client, S.settings['id_map'][subscription], readOnly=True)
             self.posts[subscription] = await client(GetHistoryRequest(peer=self.channels[subscription], limit=30,
                 offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             self.posts[subscription] = self.posts[subscription].messages
