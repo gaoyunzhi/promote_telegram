2007fb7f15821d198f57432366f5aabac2ae585d
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-02-20 18:52:22 -0800

    commit

--- telegram_promote.py
+++ telegram_promote.py
@@ -229,5 +229,5 @@ async def test():
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    r = loop.run_until_complete(test())
+    r = loop.run_until_complete(run())
     loop.close()
